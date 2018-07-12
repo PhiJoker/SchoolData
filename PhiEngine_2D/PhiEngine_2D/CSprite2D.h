ba7561b023473e90b045ab44ff0167cd2a764ab3
Kaitou <fatezerogeass@gmail.com>
2018-07-12 20:51:00 +0900

    PhiEngine
    code: Hunter
    2018/07/12

--- PhiEngine_2D/PhiEngine_2D/CSprite2D.h
+++ PhiEngine_2D/PhiEngine_2D/CSprite2D.h
@@ -8,26 +8,47 @@
 #ifndef _CSPRITE2D_H_
 #define _CSPRITE2D_H_
 
+//-----------------------------------------------------------------------------
+//	Include header files.
+//-----------------------------------------------------------------------------
 #include	"GameUtil.h"
+#include	"MathUtil.h"
 
+//-----------------------------------------------------------------------------
+//	構造体定義
+//-----------------------------------------------------------------------------
 
+// 頂点データフォーマット
+typedef struct tagVERTEX2D
+{
+	float x, y, z;
+	float u, v;
+}VERTEX2D;
 
+//=============================================================================
+//!	@class	CSprite2D
+//!	@brief	CSprite2Dクラス
+//=============================================================================
 class CSprite2D
 {
 private:
-	D3DXMATRIX		m_Mat;
+	VERTEX2D			m_v[4];			//	
 
-	D3DXVECTOR3		m_angle;
-	D3DXVECTOR3		m_trans;
+	D3DXMATRIX			m_Mat;			//	変換行列
+	D3DXMATRIX			m_MatTotal;		//	積算行列
 
+	D3DXVECTOR3			m_rotation;		//	回転角度
+	D3DXVECTOR3			m_position;		//	移動量
+	D3DXVECTOR3			m_scale;		//	スケール量
 
-	bool			m_isActive;
+	LPDIRECT3DTEXTURE9*	m_tex;			//	テクスチャ
+	bool				m_isActive;		//	
 
 public:
-	CSprite2D(LPCWSTR filename);
-	~CSprite2D();
+	CSprite2D(){}						//	コンストラクタ
+	~CSprite2D(){}						//	デストラクタ
 
-	void Init(LPDIRECT3DDEVICE9 lpdev);
+	void Init(LPDIRECT3DDEVICE9 lpdev, LPCWSTR filename, D3DXVECTOR3& position, D3DXVECTOR3& rotation, D3DXVECTOR3& scale);
 
 	void Update(LPDIRECT3DDEVICE9 lpdev);
 
@@ -38,5 +59,7 @@ public:
 };
 
 
-
+//*****************************************************************************
+//	End of file.
+//*****************************************************************************
 #endif
