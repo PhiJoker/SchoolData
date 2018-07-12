4525c44bfa2b9d4ca7468e943bfa462809d93163
Phi <fatezerogeass@qq.com>
2018-07-12 12:04:07 +0900

    PhiEngine
    Code: Phi
    2018/07/12

--- PhiEngine_2D/PhiEngine_2D/CSprite2D.cpp
+++ PhiEngine_2D/PhiEngine_2D/CSprite2D.cpp
@@ -10,27 +10,55 @@
 //-----------------------------------------------------------------------------
 #include "CSprite2D.h"
 
-CSprite2D::CSprite2D(LPCWSTR filename)
+void CSprite2D::Init(LPDIRECT3DDEVICE9 lpdev, LPCWSTR filename, D3DXVECTOR3& position, D3DXVECTOR3& rotation, D3DXVECTOR3& scale)
 {
+	m_tex = (LPDIRECT3DTEXTURE9*)malloc(sizeof(LPDIRECT3DTEXTURE9));
+	HRESULT hr;
+	hr = D3DXCreateTextureFromFile(lpdev, filename, m_tex);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, L"Err", L"Texture erro", MB_OK);
+	}
+	m_v[0] = { -1.0f,1.0f,0.0f,0,0 };
+	m_v[1] = { 1.0f,1.0f,0.0f,1,0 };
+	m_v[2] = { -1.0f,-1.0f,0.0f,0,1 };
+	m_v[3] = { 1.0f,-1.0f,0.0f,1,1 };
 
-}
-
-CSprite2D::~CSprite2D()
-{
-}
-
-void CSprite2D::Init(LPDIRECT3DDEVICE9 lpdev)
-{
+	MakeWorldMatrix(m_MatTotal, position, rotation, scale);
 }
 
 void CSprite2D::Update(LPDIRECT3DDEVICE9 lpdev)
 {
+	static D3DXVECTOR3 tmpPos = { 0.0f, 0.0f, 0.0f };
+	static D3DXVECTOR3 tmpRotate = { 0.0f, 0.0f, 0.0f };
+	static D3DXVECTOR3 tmpScale = { 1.0f, 1.0f, 1.0f };
+
+	if (GetKeyboardPress(DIK_LEFT)) {
+		tmpPos.x -= 0.1f;
+	}
+	MakeWorldMatrix(m_Mat,m_MatTotal, tmpPos,tmpRotate,tmpScale);
+
 }
 
 void CSprite2D::Draw(LPDIRECT3DDEVICE9 lpdev)
 {
+	lpdev->SetTransform(D3DTS_WORLD, &m_MatTotal);
+	lpdev->SetFVF(D3DFVF_XYZ | D3DFVF_TEX1);
+	lpdev->SetTexture(0, *m_tex);
+	lpdev->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, m_v, sizeof(VERTEX2D));
 }
 
 void CSprite2D::Exit()
 {
+
+	if (m_tex != NULL)
+	{
+		(*m_tex)->Release();
+		free(m_tex);
+		m_tex = NULL;
+	}
 }
+
+//*****************************************************************************
+//	End of file.
+//*****************************************************************************
